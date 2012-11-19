ca7193c7ed720226c0df78983b4948dcb740c1fe
Jan Dvořák <mordae@anilinux.org>
2012-11-19 20:37:00 +0100

    twilight: monitor network desired state changes
    Signed-off-by: Jan Dvořák <mordae@anilinux.org>

--- ponycloud/twilight/manager/model_manager.py
+++ ponycloud/twilight/manager/model_manager.py
@@ -21,6 +21,9 @@ class ModelManager(object):
         self.model = Model()
         self.model.load([('host', self.uuid, 'current', {'uuid': self.uuid})])
 
+        # Start watching the model for changes.
+        self.watch_model()
+
 
     def start_model_tasks(self):
         """Starts model-related periodic tasks."""
@@ -47,6 +50,7 @@ class ModelManager(object):
             new_model = Model()
             new_model.load(self.model.dump(['current']))
             self.model = new_model
+            self.watch_model()
 
         self.model.load(changes)
         self.inseq += 1
@@ -80,6 +84,57 @@ class ModelManager(object):
             'event': 'twilight-state-update',
             'changes': changes,
         })
+
+
+    def watch_model(self):
+        """Register model watches to be able to react to state changes."""
+
+        def before_nic_update(table, row):
+            if row.desired is not None and row.get('nic_name') is not None:
+                self.nic_event('deconfigure', row)
+
+        def after_nic_update(table, row):
+            if row.desired is not None and row.get('nic_name') is not None:
+                self.nic_event('configure', row)
+
+        def before_bond_update(table, row):
+            if row.desired is not None and row.get('bond_name') is not None:
+                self.bond_event('deconfigure', row)
+
+        def after_bond_update(table, row):
+            if row.desired is not None and row.get('bond_name') is not None:
+                self.bond_event('configure', row)
+
+        def before_vlan_update(table, row):
+            if row.desired is not None and row.get('vlan_name') is not None:
+                self.vlan_event('deconfigure', row)
+
+        def after_vlan_update(table, row):
+            if row.desired is not None and row.get('vlan_name') is not None:
+                self.vlan_event('configure', row)
+
+        def before_bridge_update(table, row):
+            if row.desired is not None and row.get('bridge_name') is not None:
+                self.bridge_event('deconfigure', row)
+
+        def after_bridge_update(table, row):
+            if row.desired is not None and row.get('bridge_name') is not None:
+                self.bridge_event('configure', row)
+
+        self.model['nic'].on_before_row_update(before_nic_update)
+        self.model['nic'].on_after_row_update(after_nic_update)
+
+        self.model['bond'].on_before_row_update(before_bond_update)
+        self.model['bond'].on_after_row_update(after_bond_update)
+
+        self.model['nic_role'].on_before_row_update(before_bridge_update)
+        self.model['nic_role'].on_before_row_update(before_vlan_update)
+
+        self.model['nic_role'].on_after_row_update(after_vlan_update)
+        self.model['nic_role'].on_after_row_update(after_bridge_update)
+    # /def watch_model
+
+# /class ModelManager
 
 
 # vim:set sw=4 ts=4 et:
