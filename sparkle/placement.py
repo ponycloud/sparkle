20bf9bd7e0b88e2528a35f80bf48c1be4e9970f0
Ondrej Koch <o.koch@zerusnet.com>
2014-02-05 12:54:06 +0100

    placement of nic, nic_role, bond and disk entities
    Signed-off-by: Ondrej Koch <o.koch@zerusnet.com>

--- sparkle/placement.py
+++ sparkle/placement.py
@@ -9,7 +9,6 @@ class Placement(object):
         """Remember the manager."""
         self.manager = manager
 
-
     def on_row_changed(self, old, new):
         """
         Triggered on every row change.
@@ -21,16 +20,63 @@ class Placement(object):
         else:
             print 'no placement routine for %s' % (old.table.name,)
 
-
     def on_host_changed(self, old, new):
         """
         By default, hosts are placed on "themselves" for "themselves".
         """
 
-        if new.desired is None:
-            self.manager.withdraw(new.pkey, new, new)
-        else:
+        if new.desired:
             self.manager.bestow(new.pkey, new, new)
+        else:
+            self.manager.withdraw(new.pkey, old, old)
 
+    def on_host_disk_changed(self, old, new):
+        """
+        Place configuration for actually present disks.
+        """
+
+        if new.current:
+            host = new.current['host']
+            disk = ('disk', new.current['disk'])
+            self.manager.bestow(host, disk, new)
+        else:
+            host = old.current['host']
+            disk = ('disk', old.current['disk'])
+            self.manager.withdraw(host, disk, old)
+
+    def on_nic_changed(self, old, new):
+        """
+        Place configuration for nic
+        """
+        if new.desired:
+            host = new.desired['host']
+            self.manager.bestow(host, new, new)
+        else:
+            host = old.desired['host']
+            self.manager.withdraw(host, old, old)
+
+    def on_bond_changed(self, old, new):
+        """
+        Place configuration for bond
+        """
+        if new.desired:
+            host = new.desired['host']
+            self.manager.bestow(host, new, new)
+        else:
+            host = old.desired['host']
+            self.manager.withdraw(host, old, old)
+
+    def on_nic_role_changed(self, old, new):
+        """
+        Place nic role for a host matched through the role's bond
+        """
+        if new.desired:
+            bond = self.manager.model['bond'][new.desired['bond']]
+            host = bond.desired['host']
+            self.manager.bestow(host, new, bond)
+        else:
+            bond = self.manager.model['bond'][old.desired['bond']]
+            host = bond.desired['host']
+            self.manager.withdraw(host, old, bond)
 
 # vim:set sw=4 ts=4 et:
